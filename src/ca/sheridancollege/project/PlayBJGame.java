70823f3858b00c1f5cc42a6a4b2662c4ef7810da
gabrielamoniz <monizdea@sheridancollege.ca>
2023-04-12 00:06:30 -0400

    version2

--- src/ca/sheridancollege/project/PlayBJGame.java
+++ src/ca/sheridancollege/project/PlayBJGame.java
@@ -3,23 +3,23 @@ package ca.sheridancollege.project;
 import java.util.ArrayList;
 
 public class PlayBJGame extends Game {
-
+   
     public PlayBJGame(String name) {
         super(name);
-    }
-
+        
     @Override
     public void play() {
         System.out.println("Welcome to play Blackjack Card Game!");
+       
         
-        //BJGame has a deckOfCards of 52 cards
-        GroupOfCards deckOfCards = new GroupOfCards(52); //4 suits x 13 values = 52 cards
+        BJGame has a deckOfCards of 52 cards
+       GroupOfCards deckOfCards = new GroupOfCards(52); //4 suits x 13 values = 52 cards
         int i=0;
-        for( BJCard.Suit suits : BJCard.Suit.values() ) //suits - spade 
+       for( BJCard.Suit suits : BJCard.Suit.values() ) //suits - spade 
         {
-            for(BJCard.Rank ranks: BJCard.Rank.values()) //ranks  1
-            {
-                deckOfCards.addCards(new BJCard(suits, ranks)); //spade,1,deckOfCards[1]=spade,2
+           for(BJCard.Rank ranks: BJCard.Rank.values()) //ranks  1
+           {
+               deckOfCards.addCards(new BJCard(suits, ranks)); //spade,1,deckOfCards[1]=spade,2
                 i++;
             }
         }
@@ -27,8 +27,8 @@ public class PlayBJGame extends Game {
 //        for(BJCard c : deckOfCards.getCards()){
 //            System.out.println(c.getSuit()+ " " + c.getRank());
 //        }
-        
         deckOfCards.shuffle();
+        
         //generate two random cards for player and dear
         
         // Only 1 dealer
